d00f36698a770b1d499fba4cbedaf6ad5349de27
Vitalii Efimov <g3770888@gmail.com>
2022-11-06 00:43:47 -0400

    Test from Vitalii TS_13_5 final code

--- src/test/java/com/cydeo/trycloud13/pages/FilesPage.java
+++ src/test/java/com/cydeo/trycloud13/pages/FilesPage.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -15,6 +17,8 @@ public class FilesPage {
     static WebElement chosenFile;
     WebElement element;
     static String chosenFileText;
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 5);
+
 
 
     public FilesPage() {
@@ -27,100 +31,47 @@ public class FilesPage {
     @FindBy(xpath = "//a[@class='menuitem action action-favorite permanent']")
     public WebElement favoriteOptionFromThreeDots;
 
-    @FindBy(xpath = "//span[@class='icon icon-starred']")
-    public WebElement favoriteOptionFromThreeDots2;
-
-    @FindBy(xpath = "//a[@class='nav-icon-favorites svg']")
-    public WebElement favoritesBtn;
-
     @FindBy(xpath = "(//tbody[@id='fileList'])[1]//span[@class='innernametext']")
     public List<WebElement> objectsFilesList;
-
-    @FindBy(xpath = "//a[@data-action='Favorite']//span[2]")
-    public WebElement removeFromFavorites;
-
-    public static WebElement chooseInThreeDotsOption1(String string) {
-//        String locator = "//a[@data-action='" + string + "']";
-        String locator = "//span[.='" + string + "']/..";
-        WebElement chooseInThreeDotsOption = Driver.getDriver().findElement(By.xpath(locator));
-        return chooseInThreeDotsOption;
-    }
-
-//    public  WebElement chooseInThreeDotsOption() {
-////        String locator = "//a[@data-action='" + string + "']";
-//        String locator = "//a[@class='menuitem action action-favorite permanent']";
-//        WebElement chooseInThreeDotsOption = Driver.getDriver().findElement(By.xpath(locator));
-//        if (chooseInThreeDotsOption.getText().equals("Remove from favorites")) {
-//            BrowserUtils.sleep(3);
-//            chooseInThreeDotsOption.click();
-////            actions.moveToElement(FilesPage.chooseInThreeDotsOption("Remove from favorites")).click().perform();
-//
-//
-////            elementOfTheeDots.click();
-////            actions.moveToElement(FilesPage.chooseInThreeDotsOption(string)).click().perform();
-//        }else if (chooseInThreeDotsOption.getText().equals("Add to favorites")){
-//            BrowserUtils.sleep(3);
-////            actions.moveToElement(filesPage.removeFromFavorites).click().perform();
-//            //           FilesPage.chooseInThreeDotsOption(string).click();
-////            actions.moveToElement(FilesPage.chooseInThreeDotsOption(string)).click().perform();
-////            actions.moveToElement(FilesPage.chooseInThreeDotsOption()).click().perform();
-//            chooseInThreeDotsOption.click();
-//        }
-//        return chooseInThreeDotsOption;
-//    }
 
     public static WebElement leftSideMenu(String string) {
         String locator = "//a[.='" + string + "']";
         WebElement leftSideMenu = Driver.getDriver().findElement(By.xpath(locator));
         return leftSideMenu;
     }
-
     public static WebElement actionIconsListMethod() {
         Faker faker = new Faker();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 5);
         List<WebElement> actionIconsList = Driver.getDriver().findElements(By.xpath("//a[@class='action action-menu permanent']"));
         List<WebElement> objectsFilesList = Driver.getDriver().findElements(By.xpath("(//tbody[@id='fileList'])[1]//span[@class='innernametext']"));
         int num = faker.number().numberBetween(1, actionIconsList.size());
-//        WebElement chosenFile;
         WebElement elementOfTheeDots;
         for (int i = 1; i <= actionIconsList.size(); i++) {
             if (i == num) {
                 chosenFile = objectsFilesList.get(i);
                 elementOfTheeDots = actionIconsList.get(i);
+                wait.until(ExpectedConditions.elementToBeClickable(elementOfTheeDots));
                 elementOfTheeDots.click();
             }
         }
         return chosenFile;
     }
-//    public static WebElement objectsFilesListMethod(List<WebElement> list) {
-////        String locator = "//a[@data-action='" + string + "']";
-//        String locator = "//a[.='" + string + "']";
-//        WebElement leftSideMenu = Driver.getDriver().findElement(By.xpath(locator));
-//        return leftSideMenu;
-//    }
 
     public static String user_choose_the_option() {
-        chosenFileText = FilesPage.actionIconsListMethod().getText();
-        System.out.println("chosenFileText in Files = " + chosenFileText);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 5);
+        chosenFileText = actionIconsListMethod().getText();
         String chosen = "";
-        String chosen1 = "";
         WebElement elementAddFavorites = Driver.getDriver().findElement(By.xpath("//a[@class='menuitem action action-favorite permanent']"));
-        System.out.println("element = " + elementAddFavorites.getText());
         if (elementAddFavorites.getText().equalsIgnoreCase("Remove from favorites")) {
+            wait.until(ExpectedConditions.elementToBeClickable(elementAddFavorites));
             elementAddFavorites.click();
-            System.out.println("chosenFileText is REMOVED in Files = " + chosenFileText);
-            BrowserUtils.sleep(3);
-            user_choose_the_option();
-
-
+            return user_choose_the_option();
         } else if (elementAddFavorites.getText().equals("Add to favorites")) {
-            BrowserUtils.sleep(3);
+            wait.until(ExpectedConditions.elementToBeClickable(elementAddFavorites));
             elementAddFavorites.click();
             chosen = chosenFileText;
-            chosen1 = chosenFileText;
-            System.out.println("chosen is ADDED in Files = " + chosen1);
-            return chosen1;
+            return chosen;
         }
-        System.out.println("chosen = " + chosen);
         return chosen;
     }
 }
--- src/test/java/com/cydeo/trycloud13/step_definitions/TS_13_5_AddFileToFavorites_StepDef.java
+++ src/test/java/com/cydeo/trycloud13/step_definitions/TS_13_5_AddFileToFavorites_StepDef.java
@@ -16,17 +16,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-public class TS_13_5_AddFileToFavorites_StepDef {
-    static WebElement chosenFile;
-    WebElement elementOfTheeDots;
+import java.util.List;
 
-    WebElement elementAddFavorites;
+public class TS_13_5_AddFileToFavorites_StepDef {
     String chosenFileText;
     LoginPage loginPage = new LoginPage();
-    BasePage basePage = new BasePage();
-    FilesPage filesPage = new FilesPage();
     FavoritePage favoritePage = new FavoritePage();
-    Actions actions = new Actions(Driver.getDriver());
+    FilesPage filesPage = new FilesPage();
 
 
     @Given("user on the dashboard page")
@@ -44,47 +40,39 @@ public class TS_13_5_AddFileToFavorites_StepDef {
 
     @When("the user clicks action-icon from any file on the page")
     public void the_user_clicks_action_icon_from_any_file_on_the_page() {
-
-//        chosenFileText = FilesPage.actionIconsListMethod().getText();
-//        System.out.println("chosenFileText = " + chosenFileText);
+        Faker faker = new Faker();
+        int num = faker.number().numberBetween(1, filesPage.actionIconsList.size());
+        WebElement elementOfTheeDots;
+        for (int i = 1; i <= filesPage.actionIconsList.size(); i++) {
+            if (i == num) {
+                chosenFileText = filesPage.objectsFilesList.get(i).getText();
+                elementOfTheeDots = filesPage.actionIconsList.get(i);
+                elementOfTheeDots.click();
+            }
+        }
     }
 
     @When("user choose the {string} option")
     public void user_choose_the_option(String string) {
-//        elementAddFavorites = Driver.getDriver().findElement(By.xpath("//a[@class='menuitem action action-favorite permanent']"));
-//        System.out.println("element = " + elementAddFavorites.getText());
-//        if (elementAddFavorites.getText().equalsIgnoreCase("Remove from favorites")) {
-//            elementAddFavorites.click();
-//            the_user_clicks_action_icon_from_any_file_on_the_page();
-//            BrowserUtils.sleep(3);
-//        } else if (elementAddFavorites.getText().equals("Add to favorites")){
-//            BrowserUtils.sleep(3);
-//            elementAddFavorites.click();
-//        }
-//        BrowserUtils.sleep(3);
-        chosenFileText = FilesPage.user_choose_the_option();
-        System.out.println("chosenFileText in the Steps= " + chosenFileText);
-
+        if (filesPage.favoriteOptionFromThreeDots.getText().equals(string)){
+            filesPage.favoriteOptionFromThreeDots.click();
+        }else{
+            filesPage.favoriteOptionFromThreeDots.click();
+            chosenFileText = FilesPage.user_choose_the_option();
+            }
     }
-
 
     @When("user click the {string} sub-module on the left side")
     public void user_click_the_sub_module_on_the_left_side(String string) {
-        BrowserUtils.sleep(3);
         FilesPage.leftSideMenu(string).click();
-
     }
 
     @Then("Verify the chosen file is listed on the table")
     public void verify_the_chosen_file_is_listed_on_the_table() {
-        BrowserUtils.sleep(3);
-        String getText = "2";
+        String getText = "";
         for (WebElement each : favoritePage.favoritesFiles) {
-            System.out.println("each.getText() = " + each.getText());
             if (each.getText().equalsIgnoreCase(chosenFileText)) {
-                System.out.println("chosenFile matched text = " + chosenFileText);
                 getText = each.getText();
-                System.out.println("getText = " + getText);
             }
         }
         Assert.assertEquals(chosenFileText, getText);
